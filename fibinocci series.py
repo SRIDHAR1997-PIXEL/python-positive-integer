dd19338d93ae03ace7a41b372517e0cdd2f9e5f8
SRIDHAR1997-PIXEL <64788725+SRIDHAR1997-PIXEL@users.noreply.github.com>
2020-05-15 15:50:19 +0530

    Add files via upload

---
+++ fibinocci series.py
@@ -0,0 +1,6 @@
+n=int(input("enter a number:"))
+print("the fibinocci series ")
+x,y=0,1
+while x<n:
+    print(x)
+    x,y=y,x+y
---
+++ fibnocci series- ans.py
@@ -0,0 +1,27 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/sridh/Desktop/FOR INTERNSHIP/fibinocci series.py ======
+enter a number:5000
+the fibinocci series 
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+1597
+2584
+4181
+>>> 
